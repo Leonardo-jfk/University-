97cb1e1f95ddb290a83379a76b32c40976bb8801
Leonardo-jfk <melehAmiti@proton.me>
2026-08-27 19:10:20 -0300

    [feature] 1st we ask for players' names

--- MenuCards.py
+++ MenuCards.py
@@ -1,26 +1,63 @@
 from funcCards import *
+from rich import print, console
+from rich.console import Console
+
+
+# def gamer(name):
+
+class GamerInfo:
+
+            def __init__(self, name):
+                self.name = name
+                self.score = 0
+                self.totalScore = 0
+                self.totalGames = 0
+
+            def __str__(self):
+                return self.name
+
+
+    # return GamerInfo
+
+
+
 
 def main():
+    menu = "MENU DU JEU"
+    print(f"{menu:*^30}")
 
-    print("Menu d'options: ")
-    r =  int(input("choiez quoi faire? 1-jouer 2-commencer le jeu 3-fin du jeu 4-sortir ? "))
+
+    # print("Menu d'options: ")
+    r =  int(input("choiez quoi faire? 1-difener les joeurs 2-commencer le jeu 3-fin du jeu 4-sortir ? "))
 
     if r == 1:
+        jouers = []
+        for _ in range(2):
+            # print("[bold yellow] === Donne deux prémons === [/bold yellow]")
+            playerName = input(" === Donne deux prémons === ")
+            player1 = GamerInfo(playerName)
+            jouers.append(player1)
+        # print(gamer())
+        print(jouers[0], jouers[1])
 
-        c1 = Cards()
-        print(c1)
-    if r == 2:
+    elif r == 2:
         c2 = GiveCards(2)
 
         print(c2[0], c2[1])
         main()
 
-    if r == 3:
+    elif r == 3:
         c2 = GiveCards(2)
         # c3 = compareCard(c2[0], c2[
-        c3 = compareCard(c2[0], c2[1])
+        c3 = compareCardNum(c2[0], c2[1])
 
         print(c3)
+
+    elif r == 4:
+        print("vous avez sorti!")
+        reponse = input("voulez vous continuer ?")
+        if reponse == "y":
+            main()
 
     # elif r == 4:
     #     break
--- funcCards.py
+++ funcCards.py
@@ -27,10 +27,11 @@ def GiveCards(quantite):
 
     return jouer1, jouer2
 
-def compareCard(card1, card2):
+def compareCardNum(card1, card2):
 
     puntos1, puntos2 = 0, 0
     cartaNombre1, cartaNombre2 = int(card1.nombre), int(card2.nombre)
+
 
 
     if cartaNombre1 == cartaNombre2:
@@ -44,6 +45,23 @@ def compareCard(card1, card2):
 
     return puntos1, puntos2
 
+def compareCardColor(card1, card2):
+    cardColor1, cardColor2 = card1.color, card2.color
+    card1Won, Card2Won = False, False
+
+    if cardColor1 == cardColor2:
+        return print("empataron")
+    elif cardColor1 == "ORO" and cardColor2 != "ORO":
+        card1Won = True
+        return card1Won
+
+    elif cardColor2 == "ORO" and cardColor1 != "ORO":
+        card2Won = True
+        return card2Won
+
+    else:
+        card1Won, card2Won = False, False
+        return print("empataron")
 
 
 # if __name__ == "__main__":
