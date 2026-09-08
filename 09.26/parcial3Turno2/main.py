8303c4a5ebaec4f06e1eea222546952be6b26c25
Leonardo-jfk <melehAmiti@proton.me>
2026-09-08 11:05:53 -0300

    turno 2, c) is done

--- 09.26/parcial3Turno2/main.py
+++ 09.26/parcial3Turno2/main.py
@@ -23,41 +23,103 @@ def mostrar_vector(vector):
 
 
 def mostrar_mayor(vector):
+    vectorMayors = list()
     num = int(input("Ingrese el num para comparar: "))
     for item in range(len(vector)):
         if vector[item].numeroAsiento > num:
-            print(vector[item])
+            vectorMayors.append(vector[item])
+
+    return vectorMayors
+
+
+def ordenar(vector, checker):
+    n = len(vector)
+    for i in range(n - 1):
+        ordenado = True
+        for j in range(n - i - 1):
+            # if vector[i].checker  > vector[j + 1].checker:
+            if getattr(vector[j], checker) > getattr(vector[j + 1], checker):
+                ordenado = False
+                vector[j], vector[j + 1] = vector[j + 1], vector[j]
+        if ordenado:
+            break
+
+
+def vectorSuma(vector, checker):
+
+    n = len(vector)
+    t = int(input("numero de cantidad: "))
+    vectorSuma = 20 * [0]
+    vectorSumeMayors = list()
+
+
+    for i in range(n):
+        for j in range(20):
+            if j == vector[i].paisDestino:
+                vectorSuma[j] += 1
+
+    for i in range(20):
+        if vectorSuma[i] > t:
+            print(f"Acumulator for pais destino: {i} is {vectorSuma[i]}")
+    print(vectorSuma)
+
+
+
 
 
 def main():
 
-    responce = -1
+    responce, cant_de_tickets = -1, 0
+    vector = None
 
     while responce != 0:
         print("1: cant de tickets")
         print("2: crear un arreglo")
         print("3: mostrar vector")
         print("4: mostrar tickets mayores de num")
+        print("5: importe de cada pais")
         print("0: salir")
 
         responce = int(input("Ingrese el numero: "))
 
 
         if responce == 1:
+
             cant_de_tickets = int(input("Ingrese la cantidad de tickets: "))
 
         if responce == 2:
-            vector = crear_class(cant_de_tickets)
+            if cant_de_tickets != 0:
+                vector = crear_class(cant_de_tickets)
+            else:
+                print("go to option 1")
 
         if responce == 3:
-            print(mostrar_vector(vector))
+            if vector != None:
+                print(mostrar_vector(vector))
+            else:
+                print("no creaste vector")
 
 
         ## b)
+        #mostrar los datos de todos los tickets cuyo
+        # número de asiento sea mayor a un valor num
         if responce == 4:
-            mostrar_mayor(vector)
+            if vector != None:
+                vectorMayors = mostrar_mayor(vector)
+                ordenar(vectorMayors, "codigo")
+                mostrar_vector(vectorMayors)
+            else:
+                print("no creaste vector")
 
 
+        # c)
+        # importe acumulado que se cobró por cada posible país de destino
+        #
+        if responce == 5:
+            if vector != None:
+                vectorSumas = vectorSuma(vector, "paisDestino")
+            else:
+                print("no creaste vector")
 
 
 
